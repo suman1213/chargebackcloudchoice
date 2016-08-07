553a5fd850b6521c020c2ca4f971657c3d8a809e
Amit Bansal <amitbansal26@gmail.com>
2016-08-07 23:19:30 +0530

    Refactored

--- src/main/resources/static/chargeback.js
+++ src/main/resources/static/chargeback.js
@@ -1,5 +1,8 @@
 
+
+
 var colors = [
+
      "#00ffff",
      "#f0ffff",
      "#f5f5dc",
@@ -55,8 +58,9 @@ for (var i = 0; i < colors.length; ++i){
 for (var i=0; i < labeList.length; i++) {
 var random = Math.round(Math.random() * ids.length);
 var color = ids.splice(random, 1);
-colorArray.push(color)
+colorArray.push(color[0])
 }
+console.log(colorArray);
 return colorArray;
 }
 var getMemoryUsageDetails = function(){
@@ -103,25 +107,90 @@ var getdataArray = function(data){
 };
 
 
+
 /* Function to populate chart Details */
 var populateChartDetails = function(data, id){
-	 var ctx = document.getElementById(id);
+	 var canvasId = document.getElementById(id);
 	 var colorArray = getcolorArray(data.label);
 	 var chartData =  {
      	    labels: getlabelsArray(data.label),
-         	    datasets: [
-         	        {
-         	        	data: getdataArray(data.data),
-         	            backgroundColor:colorArray,
-         	            hoverBackgroundColor: colorArray
+		         	    datasets : [ {
+			data : getdataArray(data.data),
+			backgroundColor :colorArray,
+		    hoverBackgroundColor: colorArray
          	        }]
          	};
-	 var myPieChart = new Chart(ctx,{
+	 
+	 var ctx = canvasId.getContext("2d");
+	 var midX = canvasId.width/2;
+	 var midY = canvasId.height/2;
+	 var totalValue = getTotalValue(data.data);
+	
+	 
+	 var pieChart = new Chart(canvasId,{
          type: 'pie',
          data: chartData,
          options: {
-		        responsive: false
+		        responsive: false,
+		        //onAnimationProgress:  drawSegmentValues,
+		        tooltips: {
+		            callbacks: {
+		              label: function(tooltipItem, data) {
+		              	var dataset = data.datasets[tooltipItem.datasetIndex];
+		                var total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
+		                  return previousValue + currentValue;
+		                });
+		                var currentValue = dataset.data[tooltipItem.index];
+		                var precentage = Math.floor(((currentValue/total) * 100)+0.5);         
+		                return precentage + "%";
+		              }
+		            }
+		          }
+		        
 		    }
      });
 	 
+	var radius = pieChart.outerRadius;
+	 
+	function drawSegmentValues(){
+		
+		 for(var i=0; i<pieChart.segments.length; i++) 
+		    {
+		        ctx.fillStyle="white";
+		        var textSize = canvasId.width/15;
+		        ctx.font= textSize+"px Verdana";
+		        // Get needed variables
+		        var value = pieChart.segments[i].value/totalValue*100;
+		        if(Math.round(value) !== value)
+		        	value = (pieChart.segments[i].value/totalValue*100).toFixed(1);
+		        value = value + '%';
+		        
+		        var startAngle = pieChart.segments[i].startAngle;
+		        var endAngle = pieChart.segments[i].endAngle;
+		        var middleAngle = startAngle + ((endAngle - startAngle)/2);
+
+		        // Compute text location
+		        var posX = (radius/2) * Math.cos(middleAngle) + midX;
+		        var posY = (radius/2) * Math.sin(middleAngle) + midY;
+
+		        // Text offside by middle
+		        var w_offset = ctx.measureText(value).width/2;
+		        var h_offset = textSize/4;
+
+		        ctx.fillText(value, posX - w_offset, posY + h_offset);
+		    }
+		
+	}
+/*End of draw Segment*/	
 }
+
+
+
+function getTotalValue(arr) {
+    var total = 0;
+    for(var i=0; i<arr.length; i++)
+        total += arr[i];
+    return total;
+}
+
+
