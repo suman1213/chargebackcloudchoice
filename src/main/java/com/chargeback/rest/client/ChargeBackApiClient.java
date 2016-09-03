f6a8483891dc36f5615d57ad6b3f726b99fff147
Amit Bansal <amitbansal26@gmail.com>
2016-09-04 00:57:07 +0530

    Refactored for feign client

--- src/main/java/com/chargeback/rest/client/ChargeBackApiClient.java
+++ src/main/java/com/chargeback/rest/client/ChargeBackApiClient.java
@@ -1,7 +1,5 @@
 package com.chargeback.rest.client;
 
-import static com.chargeback.constants.ChargeBackConstants.GET_ORG_LIST;
-
 import java.util.List;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
@@ -12,19 +10,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.chargeback.vo.UsageRecord;
 
-//@FeignClient("CHARGEBACK-API")
-@FeignClient(name="CHARGEBACK-API", url="http://lnar-pbbc005.corp.capgemini.com:8081")
+@FeignClient(name="CHARGEBACK-API")
 public interface ChargeBackApiClient {
 
-	@RequestMapping(value = "/getInstanceMetrics", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/metrics/getInstanceMetrics", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<UsageRecord> getAllApplicationInstanceData();
 
-	@RequestMapping(value = "/getFreeResource/{resourceType}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/metrics/getFreeResource/{resourceType}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public String getFreeResourceForResourceType(@PathVariable("resourceType") final String resourceType);
 
-	@RequestMapping(value = GET_ORG_LIST, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/metrics/getOrgList", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<String> getOrgList();
 
-	@RequestMapping(value = "/getSpaceList/{orgName}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/metrics/getSpaceList/{orgName}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<String> getSpaceList(@PathVariable("orgName") final String orgName);
 }
